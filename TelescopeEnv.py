5d0ebc0dc51d6a012b3c2296cb902f6d28552550
gmegh <guillemmegias@gmail.com>
2022-12-07 17:33:29 +0000

    Updates

--- TelescopeEnv.py
+++ TelescopeEnv.py
@@ -12,12 +12,7 @@ import time
 class Telescope(Env):
     def __init__(self, name):
         super(Telescope, self).__init__()
-        
-        # Define observation space (elevation angle, fwhm)
-        #self.observation_space = spaces.Box(low = 0.0 , 
-        #                                    high = 4.0,
-        #                                    dtype= np.float16) 
-    
+
         # Define action space corresponding to all degres of freedom
         M2hex = {'low': -np.float16(np.ones(5)*1e-2),
                     'high': np.float16(np.ones(5)*1e-2)}
@@ -41,6 +36,7 @@ class Telescope(Env):
                                                               M1M3bend['high'] )), 
                                         dtype = np.float16)
 
+        # Define observation space to be the altitude and the reward
         self.observation_space = spaces.Box(low =  np.float16(np.array([0,0])), 
                                         high =  np.float16(np.array([30,30])), 
                                         dtype = np.float16)
@@ -67,14 +63,7 @@ class Telescope(Env):
         self.closeLoopTask.batoidCmpt.applyLUT()
         
 
-    def plot_reward_evolution(self):
-        # Init the canvas 
-        plt.plot(np.arange(len(self.rewards)), self.rewards)
-        plt.tick_params(labelsize = 16)
-        plt.ylabel('FWHM', fontsize = 20)
-        plt.xlabel('Iteration', fontsize = 20)
-        plt.draw()
-
+    # Optimal SAC baseline
     def optimal(self):
         gqEffFwhm, wfe, sensor_ids = self.closeLoopTask.fwhm_compute(self.ep_return, self.obsId, self.altitude, rotAngInDeg=0.0)
 
@@ -97,8 +86,7 @@ class Telescope(Env):
         self.altitude = 27.0912
         self.obsId = 9006000
 
-
-        # Define and initialize closed loop task
+        # Define and reinitialize telescope
         '''
         self.closeLoopTask = CloseLoopTask()    
         args = argparse.Namespace(inst='comcam', filterType='ref', 
@@ -113,13 +101,12 @@ class Telescope(Env):
         self.closeLoopTask.initializeRealLoop(args)
         '''
 
-        # Set Altitude
+        # Set Altitude and apply LUT
         self.closeLoopTask.batoidCmpt.setSurveyParam(
             obsId=self.obsId,
             zAngleInDeg=self.altitude,
             rotAngInDeg=0.0
         )
-
         self.closeLoopTask.batoidCmpt.setZAngle()
         self.closeLoopTask.batoidCmpt.applyLUT()
 
@@ -127,16 +114,13 @@ class Telescope(Env):
         self.closeLoopTask.batoidCmpt.setDofInUm(self.dof_initial)
 
         # Save the DOF file
-        #self.closeLoopTask.batoidCmpt.saveDofInUmFileForNextIter(self.dof_initial)
+        self.closeLoopTask.batoidCmpt.saveDofInUmFileForNextIter(self.dof_initial)
 
         # Reward for executing a step.
         reward = self.closeLoopTask.fwhm_compute(self.ep_return, self.obsId, self.altitude, rotAngInDeg=0.0)
 
-        # Draw elements on the canvas
-        #self.plot_reward_evolution()
-
         # return the observation
-        return (self.altitude, 0.0)
+        return (self.altitude, -reward)
 
 
     def step(self, action):
@@ -145,7 +129,6 @@ class Telescope(Env):
         
         # Assert that it is a valid action 
         action = np.float16(action)
-        #assert self.action_space.contains(action), "Invalid Action"
 
         # Perform action
         self.closeLoopTask.batoidCmpt.setZAngle()
@@ -153,30 +136,17 @@ class Telescope(Env):
         self.closeLoopTask.batoidCmpt.setDofInUm(action)
 
         # Save the DOF file
-        #self.closeLoopTask.batoidCmpt.saveDofInUmFileForNextIter(action)
+        self.closeLoopTask.batoidCmpt.saveDofInUmFileForNextIter(action)
 
         # Reward for executing a step.
         reward, _, _ = self.closeLoopTask.fwhm_compute(self.ep_return, self.obsId, self.altitude, rotAngInDeg=0.0)
 
         # Update next iteration observation
         self.obsId += 10
-        #self.altitude = 27.0912
 
         dofInUm = self.closeLoopTask.batoidCmpt.getDofInUm()
-
-        # Set Altitude
-        #self.closeLoopTask.batoidCmpt.setSurveyParam(
-        #    obsId=self.obsId,
-        #    zAngleInDeg=self.altitude,
-        #    rotAngInDeg=0.0
-        #)
-
-        #self.closeLoopTask.batoidCmpt.setZAngle()
 
         # Increment the episodic return
         self.ep_return += 1
 
-        # Draw elements on the canvas
-        #self.plot_reward_evolution()
-
-        return (self.altitude, 0.0), -reward, done, []
+        return (self.altitude, -reward), -reward, done, []
